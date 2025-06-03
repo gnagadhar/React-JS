ae1259c1a0b14f1a8cdb7e7868f94e4dfe9caa79
gnagadhar kalshetti <gangadharkalshetti16@gmail.com>
2025-06-03 11:48:11 +0530

    add counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,34 +9,46 @@ function App() {
   let [counter, setcounter] = useState(0)
 
   const Addvalue = () => {
-     
-    if (counter <= 19 ) {
+
+    if (counter <= 19) {
       setcounter(counter + 1)
-    }else{
+    } else {
       setcounter(counter)
     }
   }
 
   const decresevalue = () => {
-     if (counter == 0 ) {
+    if (counter == 0) {
       setcounter(counter)
-    }else{
+    } else {
       setcounter(counter - 1)
     }
   }
- 
+
   return (
     <>
-     <h1>gangadhar</h1>
-     <h2>conter value: {counter}</h2>
+    <div className='flex justify-center mt-18'>
+      <div className='flex justify-center  bg-blue-200 rounded-4xl h-80 w-100 m-4'>
+        <div className=''>
+          <div className='flex justify-center'>
+           <h1 className='font-bold text-2xl m-4'>Counter</h1>
+          </div>
+          <div className='flex justify-center'>
+           <h2 className='font-semibold text-xl m-4'>conter value: {counter}</h2>
+          </div>
 
-     <button onClick={Addvalue}
-     >Add value{counter}</button>
-     <br />
-     <button onClick={decresevalue}
-     >decrese value{counter}</button>
-
-     <p>{counter}</p>
+          <div className='flex justify-center'>
+            <button className='m-4 border border-blue-300 rounded-3xl p-2 px-6 text-white bg-blue-300' onClick={Addvalue}
+            >Add value</button>
+            <button className='m-4 ml-2 border border-red-300 rounded-3xl p-2 text-white bg-red-300' onClick={decresevalue}
+            >decrese value</button>
+         </div>
+         <div className='flex justify-center'>
+           <h1 className='m-5 text-5xl'>{counter}</h1>
+         </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
